495bfe107e36627d9d6a3fbb52747c010f7b9f6c
valeo.popov <valeo.popov@gmail.com>
2019-11-07 12:32:06 +0300

    apartments (too slow in java, but concept is working)

--- src/Apartments.java
+++ src/Apartments.java
@@ -46,23 +46,6 @@ public class Apartments {
 
         int cnt = 0;
 
-//        for (long x : a) {
-//            long lb = x - k;
-//            long ub = x + k;
-//            int idx = getLowerBoundIdx(b, lb);
-//            if (idx != -1 && b[idx] <= ub) {
-//                cnt++;
-//                // delete element from array
-//                int[] b2 = new int[b.length-1];
-//                for (int j = 0; j < idx; j++) {
-//                    b2[j] = b[j];
-//                }
-//                for (int j = idx+1; j < b.length; j++) {
-//                    b2[j-1] = b[j];
-//                }
-//                b = b2;
-//            }
-//        }
         int lastIdx = 0;
         for (int i = 0; i < a.length; i++) {
             long lb = a[i] - k;
@@ -108,5 +91,53 @@ public class Apartments {
             }
         }
     }
+
+    /* working c++ solution
+    *
+    * #include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+bool apart(ll *des, ll *siz, ll i, ll j, ll k)
+{
+	if(siz[j]<=(des[i]+k)&&siz[j]>=(des[i]-k))
+		return true;
+	else
+		return false;
+}
+
+int main(){
+	ll n,m,k,i,j;
+	cin>>n>>m>>k;
+	ll des[n]; des[0]=0;//desired size
+	ll siz[m]; siz[0]=0;//size of app
+	ll num=0;
+
+	for(ll i=1;i<=n;i++){cin>>des[i];}//1 indexing
+	for(ll i=1;i<=m;i++){cin>>siz[i];}//1 indexing
+	sort(des,des+n+1);
+	sort(siz,siz+m+1);
+
+	i=1; //Set i=1 initially
+	for(ll j=1;j<=m;j++){		//Iterate for size
+		for(;i<=m;i++){		//Iterate for desired keeps checking next
+			if(apart(des,siz,i,j,k)==true){
+				num++;			//Accept if in range
+				i++;			//Increment deired
+				break;
+				}
+			else{
+				if(siz[j]<des[i]-k)	//If size not reachable
+					break;
+				else
+					continue;		//Increment desired
+			}
+		}
+	}
+
+	cout<<num;
+}
+    *
+    * */
 
 }
