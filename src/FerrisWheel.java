8132f6dc45051a57c46ab84b683a28beebca8647
valeo.popov <valeo.popov@gmail.com>
2019-11-08 14:12:24 +0300

    ferris wheel correct and fast

--- src/FerrisWheel.java
+++ src/FerrisWheel.java
@@ -58,58 +58,20 @@ public class FerrisWheel {
         // sort for binary search
         w.sort(Integer::compareTo);
 
-        // use binary search to find minimal number of gondolas
-        int p = -1;
-        for (int b = n; b >= 1; b /= 2) {
-            while (!ok(w, x, p+b)) p += b;
-        }
-        int k = p+1;
-
-        System.out.println(k);
-    }
-
-    static boolean ok(final List<Integer> list, final int x, final int baskets) {
-        int ans = 0;
-        List<Integer> w = new ArrayList<>(list);
-        while (w.size() > 0) {
-            int large = w.get(w.size()-1);
-            w.remove(w.size()-1);
-            if (w.size()>0) {
-                int ubIdx = upperBound(w, x - large);
-                if (ubIdx != 0) {
-                    w.remove(ubIdx - 1);
-                }
-            }
-            ans++;
-            if (ans > baskets) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /** Works like C++ upper_bound
-     * Get index of element more than value */
-    private static <T> int upperBound(List<? extends Comparable<? super T>> list, T value) {
-        final int size = list.size();
-        int start = 0;
-        int end = size-1;
-        int mid = (start + end) >>> 1;
-        while (true) {
-            int cmp = list.get(mid).compareTo(value);
-            if (cmp == 0 || cmp < 0) {
-                start = mid + 1;
-                if (end < start)
-                    return mid < size - 1 ? mid + 1 : size;
+        int i = 0;
+        int j = w.size()-1;
+        int cnt = 0;
+        while (i <= j) {
+            if (w.get(i) + w.get(j) <= x) {
+                i++;
+                j--;
             } else {
-                end = mid - 1;
-                if (end < start)
-                    return mid;
+                j--;
             }
-            mid = (start + end) >>> 1;
+            cnt++;
         }
+        System.out.println(cnt);
+
     }
-
-
 
 }
